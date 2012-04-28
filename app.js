8174e2e6b4e5bec86f8ff6ac8890bc10835eb548
Memo Zhou <justmiaotou@gmail.com>
2012-04-28 20:26:08 +0800

    still testing...

--- app.js
+++ app.js
@@ -4,15 +4,29 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+  , jade = require('jade')
+  , mustache = require('mustache');
 
 var app = module.exports = express.createServer();
 
 // Configuration
 
+// 使mustache能够成为express的渲染引擎
+mustache.compile = function(str, opt) {
+    return function(locals) {
+        console.log(locals);
+        return mustache.to_html(str, opt);
+    };
+};
+
 app.configure(function(){
   app.set('views', __dirname + '/views');
+  // 无需后缀
   app.set('view engine', 'jade');
+  // 映射后缀为.mu的文件由mustache来渲染
+  // app.register('.mu', require("mustache");
+  app.register('.mu', mustache);
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -28,8 +42,9 @@ app.configure('production', function(){
 });
 
 // Routes
-
 app.get('/', routes.index);
+app.get('signup', routes.signup);
+app.post('signup', routes.signup_post);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 
-/*
+/**
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
-};
+  res.render('index', { title: 'Express' });
+
+/**
+ * 注册页
+ */
+exports.signup = function(req, res) {
+    var opt = {};
+    res.render('signup', opt);
+}
+exports.signup_post = function(req, res) {
+}
